18d832fc3673b55b1d988c73c82f8b4f8781f723
AlexaSky <alexasky111@gmail.com>
2022-05-10 11:18:56 -0400

    5/10/2022

--- myPractices/interview_tasks/from_generalpdf/Task20_21_22.java
+++ myPractices/interview_tasks/from_generalpdf/Task20_21_22.java
@@ -1,6 +1,6 @@
 package interview_tasks.from_generalpdf;
 
-public class Task20 {
+public class Task20_21_22 {
     /*
     Write a function:
 that, given a positive integer N, prints the consecutive numbers from 1 to N,
@@ -29,7 +29,9 @@ TestCoders
 Codility
      */
     public static void main(String[] args) {
-        codilityTest(30);
+//        codilityTest(30);
+//        System.out.println(factorial(3));
+        System.out.println(convertToStr(-1234556));
     }
 
     public static void codilityTest(int number) {
@@ -45,5 +47,53 @@ Codility
                 System.out.println(num);
             }
         }
+    }
+
+    public static int factorial(int num) {
+        int n = 1;
+        for (int j = 1; j < num; j++) {
+            n *= (j + 1);
+        }
+        return n;
+    }
+
+    /*
+    //22
+Numbers -- Prime Number. Prime num is a number that divided by itself and 1
+Write a method that can check if a number is prime or not
+     */
+    public static boolean isPrime(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        return (count == 2);
+    }
+
+    /*
+    //23
+int num=-1234556;
+-6554321
+     */
+    public static String convertToStr(int num) {
+
+        String str = String.valueOf(num);
+        String res = "";
+
+        if (str.charAt(0) == '-') {
+            res += str.charAt(0);
+            for (int i = str.length() - 1; i >= 1; i--) {
+                res += str.charAt(i);
+            }
+        } else {
+            res = "";
+
+            for (int i = str.length() - 1; i >= 0; i--) {
+                res += str.charAt(i);
+            }
+        }
+        return res;
     }
 }
