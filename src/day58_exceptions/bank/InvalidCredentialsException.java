4640157f9e68ff470175956ba80de084bb0b1caa
AlexaSky <alexasky111@gmail.com>
2022-05-15 19:39:03 -0400

    5/15/2022

--- src/day58_exceptions/bank/InvalidCredentialsException.java
+++ src/day58_exceptions/bank/InvalidCredentialsException.java
@@ -1,5 +1,10 @@
 package day58_exceptions.bank;
 
 public class InvalidCredentialsException extends Exception{
+    //this is checked exception now because the parent is the Exception Class
+
+    public InvalidCredentialsException(String msg){
+        super(msg);
+    }
 
 }
--- src/day58_exceptions/bank/NotEnoughMoneyException.java
+++ src/day58_exceptions/bank/NotEnoughMoneyException.java
@@ -2,6 +2,7 @@ package day58_exceptions.bank;
 
 public class NotEnoughMoneyException extends RuntimeException{
 
+    //this is unchecked exception now because the parent is the RuntimeException Class
     public NotEnoughMoneyException(){
         super("Not Enough money in the account");
     }
